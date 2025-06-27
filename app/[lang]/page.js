67ef45ccda8a9bfa29b03e9979fecaf3c7a34644
Lilian <95133564+lilski190@users.noreply.github.com>
2025-06-27 13:13:03 +0200

    Update page.js

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -13,7 +13,6 @@ export default async function Home({ params }) {
 
   return (
     <div className="bg-base-200">
-      {JSON.stringify(dict)}
       <div className="hero min-h-screen">
         <div className="hero-overlay"></div>
         <div className="hero-content text-neutral-content text-center">
