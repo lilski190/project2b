6fb400e7e7812f1a247ee1203bd3d6ee6b5dfea1
Lilian <lilidrabi@gmail.com>
2025-06-27 13:24:16 +0200

    update glopbals path

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "../../project2b/app/[lang]/globals.css";
+import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
